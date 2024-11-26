c024da6938462f84c927fbdb8973bcadf334278b
NilMG <116452199+NilMG@users.noreply.github.com>
2024-11-26 15:06:58 -0800

    Update computed_columns.py

--- psh/computed_columns.py
+++ psh/computed_columns.py
@@ -41,7 +41,7 @@ def compute_remoteness(row: pandas.Series) -> pandas.Series:
     reservoir_lon = row["Longitude"]
     
     # Fetch nearby grocery stores (the treshold is defined in the function)
-    grocery_stores = fetch_grocery_stores(GOOGLE_MAPS_API_KEY, reservoir_lat, reservoir_lon)
+    grocery_stores = fetch_grocery_stores(AIzaSyD8G_C8RknpUBgPTiNerZkOQ5W_1ge5ASk, reservoir_lat, reservoir_lon)
 
     # If no grocery stores are found, mark as remote
     row["Remote"] = grocery_stores.empty
